0d096d435117ebe0368a9dae2b6b82c5ebe51135
Alpharn <arkrond@gmail.com>
2023-09-17 18:38:22 +0300

    refactor: adjust LoginPage instance and update tests

--- features/login.demo.feature
+++ features/login.demo.feature
@@ -2,5 +2,5 @@ Feature: Saucedemo Login Validation
 
   Scenario: User should see an error when clicking Login without entering a username
     Given User is located on the main page of saucedemo website
-    When User clicks the "Login" button
+    When User clicks the Login button
     Then User should see "Epic sadface: Username is required" error message
--- features/step-definitions/login.demo.steps.js
+++ features/step-definitions/login.demo.steps.js
@@ -1,15 +1,15 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import LoginPage from '../pageobjects/login.page.js';
+import loginPage from '../pageobjects/login.page.js';
 
 Given(/^User is located on the main page of saucedemo website$/, async () => {
     await browser.url("https://www.saucedemo.com/");
 });
 
-When(/^User clicks the "([^"]*)" button$/, async (buttonName) => {
-    await LoginPage.clickLoginButton();
+When(/^User clicks the Login button$/, async () => {
+    await loginPage.clickLoginButton();
 });
 
 Then(/^User should see "([^"]*)" error message$/, async (expectedErrorMessage) => {
-  const actualErrorMessage = await LoginPage.getErrorMessageText();
+  const actualErrorMessage = await loginPage.getErrorMessageText();
   expect(actualErrorMessage).toBe(expectedErrorMessage);
 });
